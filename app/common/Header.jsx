8ff39ef19d89429cfbdfbae7045d4ab85313ec68
Mahedi-Hasan10 <hasanmdmahedi193@gmail.com>
2023-12-11 12:15:06 +0600

    hero updated

--- app/common/Header.jsx
+++ app/common/Header.jsx
@@ -17,7 +17,7 @@ const Header = () => {
   };
 
   return (
-    <header className=" relative flex lg:gap-[107px] md:gap-[50px] gap-[40px] justify-between lg:px-[86px] md:px-[70px] px-[30px] py-[40px] text-white bg-[#0063a4]">
+    <header className=" relative flex lg:gap-[107px] md:gap-[50px] gap-[40px] justify-between lg:px-[86px] md:px-[70px] px-[30px] py-[30px] text-white bg-[#0063a4] z-50">
       {/* logo */}
       <div className="flex items-center justify-center">
         <Link href="/"><Image
@@ -30,11 +30,11 @@ const Header = () => {
       {/* navbar */}
       <div className="lg:flex gap-[60px] md:hidden hidden">
         <nav className="flex gap-[60px] text-base items-center justify-center">
-          <Link className="font-bold" href="/">
+          <Link className="font-bold" href="/courses">
             Courses
           </Link>
-          <Link href="/">About StackJunior</Link>
-          <Link href="/">Quick Tutorials</Link>
+          <Link href="/about">About StackJunior</Link>
+          <Link href="/tutorials">Quick Tutorials</Link>
         </nav>
 
         {/* account and learning button */}
@@ -46,12 +46,12 @@ const Header = () => {
         </div>
       </div>
       {showMenu && (
-        <div className="flex flex-col py-5 space-y-3 gap-[40px] w-[100%] transition duration-300 absolute top-[127px] left-0 bg-[#0063a4] bg-opacity-80 text-base justify-start items-center">
-          <Link className="font-bold mt-2" href="/">
+        <div className="flex flex-col py-5 space-y-3 gap-[40px] w-[100%] transition duration-300 absolute top-[110px] left-0 bg-[#0063a4] bg-opacity-80 text-base justify-start items-center">
+          <Link className="font-bold mt-2" href="/courses">
             Courses
           </Link>
-          <Link href="/">About StackJunior</Link>
-          <Link href="/">Quick Tutorials</Link>
+          <Link href="/about">About StackJunior</Link>
+          <Link href="/tutorials">Quick Tutorials</Link>
           <button className="font-bold">My Account</button>
           <button className="px-6 py-3 bg-[#ff0b80] text-[18px] font-bold rounded-[43px] shadow-sm">
             Start Learning
--- app/components/hero/hero.jsx
+++ app/components/hero/hero.jsx
@@ -3,8 +3,8 @@ import Image from "next/image"
 const Hero = () => {
     const searchIcon = <IoSearch/>
     return (
-        <div className="bg-[#0063A4] grid lg:grid-cols-2 grid-cols-1 lg:pt-[80px] pt-[40px]  lg:px-[86px] px-[50px] lg:pb-0 pb-10">
-            <div className=" flex flex-col order-1 gap-3 ">
+        <div className="bg-[#0063A4] relative flex flex-wrap lg:pt-[150px] pt-[40px]  lg:px-[86px] px-[50px] lg:pb-0 pb-10">
+            <div className=" flex flex-col order-1 gap-3 py-10 z-10 max-w-[506px]">
                 <h2 className="text-white lg:text-[64px] ml-5 lg:font-bold md:text-[50px] md:font-semibold text-[40px] font-medium ">Learn on your schedule</h2>
             <div className="flex gap-3 justify-center max-w-[80%] mx-auto pb-[50px]">
                 <input type="search" placeholder="Search your desire courses" className=" rounded-md shadow-md lg:py-6 py-4 lg:pl-20 lg:pr-[150px] md:pr-[80px] pr-[10px]"/>
@@ -13,8 +13,8 @@ const Hero = () => {
                 </button>
             </div>
             </div>
-            <div className="order-last lg:flex hidden">
-                <Image src="/hero/hero.png" width={1190} height={660} alt="hero-baner" />
+            <div className="order-last lg:flex absolute bottom-0 right-0 justify-start hidden z-0">
+                <Image src="/hero/hero.png" width={1000} height={600} alt="hero-baner"/>
             </div>
         </div>
     );
